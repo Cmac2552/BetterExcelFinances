d073a65ddc0a00d1085be2f381f93ffbaac36be7
Colin MacMaster <colinmac1004@gmail.com>
2025-03-09 20:02:20 -0400

    adding the ability to delete a section

--- app/components/financialSections.tsx
+++ app/components/financialSections.tsx
@@ -1,5 +1,13 @@
 import { useEffect, useState } from "react";
 import CurrencyInput from "react-currency-input-field";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export type FinancialSectionData = {
   id: number;
@@ -20,12 +28,14 @@ interface FinancialSectionProps {
   section: FinancialSectionData;
   open: boolean;
   onSectionModify: (data: any) => void;
+  sectionDelete: (data: number) => void;
 }
 
 export default function FinancialSection({
   section,
   open,
   onSectionModify,
+  sectionDelete,
 }: FinancialSectionProps) {
   const [sectionData, setSectionData] = useState(section);
   useEffect(() => {
@@ -48,17 +58,11 @@ export default function FinancialSection({
       return;
     }
     try {
-      const response = await fetch("/api/section", {
+      const response = await fetch("/api/section/", {
         method: "PATCH",
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({
-          parentId: section.id,
-          sectionLabel: value.label,
-          id: value.id,
-          value: newValue,
-        }),
       });
       if (response.ok) {
         const responseData = await response.json();
@@ -75,50 +79,75 @@ export default function FinancialSection({
     }
   };
 
+  const onSectionDelete = async () => {
+    const response = await fetch("/api/section/" + sectionData.id, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (!response.ok) {
+      console.log("damn that sucks");
+    }
+    sectionDelete(sectionData.id);
+  };
+
   return (
-    <div className="my-4 w-1/3">
-      <div className="w-full flex">
-        <span className="text-white text-2xl max-w-[85%] whitespace-nowrap overflow-hidden text-ellipsis">
-          {section.title}
-        </span>
-        <span className="text-white text-2xl">
-          ${sectionValue.toLocaleString()}
-        </span>
-      </div>
-      <div className="w-full grid grid-cols-1">
-        <div className="flex flex-col justify-center w-full">
-          {open &&
-            sectionData.values.map(
-              (value: FinancialSectionItemData, index: number) => (
-                <div
-                  key={index}
-                  className="grid grid-cols-[4%_30%_55%_11%] w-full"
-                >
-                  <div></div>
-                  <div className="flex items-center ">
-                    <label
-                      className="text-white mr-2"
-                      key={index + value.label}
+    <div className="my-4 w-1/3 h-full">
+      <Card className="bg-black h-full w-[95%]">
+        <CardHeader className="p-2">
+          <div className="w-full flex">
+            <CardTitle>
+              <span className="text-white text-2xl max-w-[85%] whitespace-nowrap overflow-hidden text-ellipsis">
+                {section.title}
+              </span>
+              <span className="text-white text-2xl">
+                ${sectionValue.toLocaleString()}
+              </span>
+            </CardTitle>
+          </div>
+        </CardHeader>
+        {open && (
+          <CardContent>
+            <div className="w-full grid grid-cols-1">
+              <div className="flex flex-col justify-center w-full">
+                {sectionData.values.map(
+                  (value: FinancialSectionItemData, index: number) => (
+                    <div
+                      key={index}
+                      className="grid grid-cols-[4%_30%_55%_11%] w-full"
                     >
-                      {value.label}
-                    </label>
-                  </div>
-                  <CurrencyInput
-                    key={index + value.label + "currentyInput"}
-                    id={value.label}
-                    name={value.label}
-                    placeholder="Please enter a number"
-                    defaultValue={value.value}
-                    decimalsLimit={2}
-                    prefix="$"
-                    className="w-[90%] px-4 py-2 bg-gray-800 text-white border border-gray-600 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 placeholder-gray-400 my-1 mx-[10%]"
-                    onBlur={(event) => handleItemValueChange(event, value)}
-                  />
-                </div>
-              )
-            )}
-        </div>
-      </div>
+                      <div></div>
+                      <div className="flex items-center ">
+                        <label
+                          className="text-white mr-2"
+                          key={index + value.label}
+                        >
+                          {value.label}
+                        </label>
+                      </div>
+                      <CurrencyInput
+                        key={index + value.label + "currentyInput"}
+                        id={value.label}
+                        name={value.label}
+                        placeholder="Please enter a number"
+                        defaultValue={value.value}
+                        decimalsLimit={2}
+                        prefix="$"
+                        className="w-[90%] px-4 py-2 bg-gray-800 text-white border border-gray-600 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:border-indigo-500 placeholder-gray-400 my-1 mx-[10%]"
+                        onBlur={(event) => handleItemValueChange(event, value)}
+                      />
+                    </div>
+                  )
+                )}
+              </div>
+            </div>
+            <div className="flex justify-end text-white w-full">
+              <button onClick={onSectionDelete}>X</button>
+            </div>
+          </CardContent>
+        )}
+      </Card>
     </div>
   );
 }
