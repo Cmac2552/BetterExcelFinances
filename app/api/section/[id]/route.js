5f193d5faa94b2f98130667803864a3fda108f49
Colin MacMaster <colinmac1004@gmail.com>
2025-03-20 21:09:27 -0400

    adding the ability to update a whole section

--- app/api/section/[id]/route.js
+++ app/api/section/[id]/route.js
@@ -16,35 +16,65 @@ export async function DELETE(request, {params}) {
     }
 }
 
-export async function PUT(request, {params}) {
+async function updateSectionItems(fieldNames, fieldValues, existingSectionItems, sectionId) {
+    const updates = fieldNames.map((fieldName, index) => {
+        const existingValue = existingSectionItems.find(
+            (ev) => ev.label === fieldName
+        );
+        const fieldValue = fieldValues[index];
+
+        if (existingValue) {
+            return prisma.sectionItem.update({
+                where: { id: existingValue.id },
+                data: { value: fieldValue },
+            });
+        } else {
+            return prisma.sectionItem.create({
+                data: { label: fieldName, value: fieldValue, sectionId: sectionId },
+            });
+        }
+    });
+    return await Promise.all(updates);
+}
+
+
+export async function PUT(request, { params }) {
     try {
         const { id } = params;
         const data = await request.json();
         const session = await getServerSession(authOptions);
 
-        const submissionData = JSON.parse(JSON.stringify(data))
+        const submissionData = JSON.parse(JSON.stringify(data));
         submissionData.userId = session.user.id;
-        
-        submissionData.values = {create:[]}
-        submissionData.fieldNames.forEach((name, index)=>{
-            submissionData.values.update.push({label:name, value:data.fieldValues[index]})
-        })
+
+        const fieldNames = submissionData.fieldNames;
+        const fieldValues = submissionData.fieldValues;
         delete submissionData.fieldNames;
         delete submissionData.fieldValues;
-        const newSection = await prisma.section.update({
-            where:{
-                id: parseInt(id)
-            },
-            data:submissionData, 
-            include: {
-                values: true,
-              },
+
+        const section = await prisma.section.findUnique({
+            where: { id: parseInt(id) },
+            include: { values: true },
         });
 
-        return new Response(JSON.stringify(newSection), {status: 200})
-    }
-    catch (error) {
+        const existingSectionItems = section.values;
+
+        await updateSectionItems(fieldNames, fieldValues, existingSectionItems, parseInt(id));
+
+        delete submissionData.values;
+
+        const updatedSection = await prisma.section.update({
+        where: { id: parseInt(id) },
+        data: submissionData,
+        include: { values: true },
+        });    
+
+        return new Response(JSON.stringify(updatedSection), { status: 200 });
+    } catch (error) {
         console.log(error);
-        return new Response(JSON.stringify({error:"Error delete Section"}), {status: 500})
+        return new Response(
+            JSON.stringify({ error: "Error updating Section" }),
+            { status: 500 }
+        );
     }
 }
