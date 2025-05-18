a63849bce5a9204c9d3b3c326c0d1d2554511a14
Colin MacMaster <colinmac1004@gmail.com>
2025-05-18 13:46:54 -0400

    moving a bunch of components around and working through some minor ui fixes

--- app/components/financialInputs.tsx
+++ app/components/financialInputs.tsx
@@ -1,44 +1,19 @@
 "use client";
-import NewSection from "./SectionModal";
-import DateInput from "./dateInput";
-import FinancialSection, {
-  FinancialSectionData,
-  FinancialSectionItemData,
-} from "./financialSections";
-import { useState } from "react";
+import FinancialSection, { FinancialSectionData } from "./financialSections";
 
 interface FinancialInputProps {
   sections: FinancialSectionData[];
-  onMonthChange: (data: string) => void;
-  onSectionAddition: (data: FinancialSectionData) => void;
   date: Date;
   setSections: (data: any[]) => void;
+  allSectionsOpen: boolean;
 }
 
 export default function FinancialInputs({
   sections,
-  onMonthChange,
-  onSectionAddition,
   date,
   setSections,
+  allSectionsOpen,
 }: FinancialInputProps) {
-  const [allSectionsOpen, setAllSectionsOpen] = useState(false);
-  const handleRefresh = async () => {
-    const response = await fetch("/api/table-data", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        date: date,
-        sectionValue: gatherDataForMonth(),
-      }),
-    });
-    if (response.ok) {
-      console.log("ok");
-    }
-  };
-
   const importMonth = async () => {
     const response = await fetch(`api/copy-month?date=${date}`, {
       method: "GET",
@@ -64,43 +39,8 @@ export default function FinancialInputs({
     setSections(updatedSections);
   };
 
-  const gatherDataForMonth = () => {
-    return sections.reduce(
-      (finalValue: number, sectionData: FinancialSectionData) =>
-        finalValue +
-        sectionData.values.reduce(
-          (finalValue: number, currentValue: FinancialSectionItemData) =>
-            sectionData.assetClass === "ASSET"
-              ? finalValue + currentValue.value
-              : -1 * (finalValue + currentValue.value),
-          0
-        ),
-      0
-    );
-  };
   return (
     <div className="flex flex-col justify-around ml-4">
-      <div className="flex items-center gap-x-4">
-        <div className="grid-cols-2">
-          <DateInput onMonthChange={onMonthChange} />
-          <NewSection
-            date={date}
-            onSectionAddition={onSectionAddition}
-            modalTitle="Add Account"
-            trigger={
-              <button className="rounded-full w-10 h-10 inline-flex items-center justify-center text-sm font-medium bg-[#f4f0e1] text-black hover:bg-[#f4f0e1] m-4 hover:border-white/20 shadow-[0_0_15px_rgba(255,255,255,0.25)] hover:shadow-[0_0_20px_rgba(255,255,255,0.4)] transition-all duration-300">
-                +
-              </button>
-            }
-          />
-        </div>
-        <button
-          className="bg-[#f4f0e1] text-black px-6 py-2 rounded-lg font-medium border border-transparent hover:border-white/20 shadow-[0_0_15px_rgba(255,255,255,0.25)] hover:shadow-[0_0_20px_rgba(255,255,255,0.4)] transition-all duration-300 mr-4 ml-auto"
-          onClick={() => setAllSectionsOpen(!allSectionsOpen)}
-        >
-          Toggle All Sections
-        </button>
-      </div>
       <div className="flex flex-wrap w-full">
         {sections?.length === 0 && (
           <div className="w-full flex justify-center">
