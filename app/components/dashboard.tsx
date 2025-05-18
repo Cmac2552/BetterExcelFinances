f7c4a379a5146c2b2c78dcfe72f3e36b43367ce0
Colin MacMaster <colinmac1004@gmail.com>
2025-05-18 13:46:54 -0400

    moving a bunch of components around and working through some minor ui fixes

--- app/components/dashboard.tsx
+++ app/components/dashboard.tsx
@@ -9,7 +9,11 @@ import {
 import { signOut } from "next-auth/react";
 import { Tailspin } from "ldrs/react";
 import "ldrs/react/Tailspin.css";
+import NewSection from "./SectionModal";
+import DateInput from "./dateInput";
 
+//currently god component
+//NEEDS REFACTOR
 export default function Dashboard() {
   const [data, setData] = useState<FinancialSectionData[]>([]);
   const [date, setDate] = useState(() => {
@@ -17,7 +21,8 @@ export default function Dashboard() {
     return new Date(Date.UTC(today.getUTCFullYear(), today.getUTCMonth(), 1));
   });
   const [loading, setLoading] = useState(true);
-  const [chartLoading, setChartLoading] = useState(true);
+  const [tableData, setTableData] = useState<any[]>([]);
+  const [allSectionsOpen, setAllSectionsOpen] = useState(false);
   const previousDateRef = useRef<Date>(null);
 
   const handleMonthChange = (monthInput: string) => {
@@ -44,31 +49,62 @@ export default function Dashboard() {
     setData((currentData) => {
       const combinedData = [...currentData, newSection];
       const sortedNewData = sortSections(combinedData);
-      setTableData(sortedNewData);
+      updateTableData(sortedNewData);
       return sortedNewData;
     });
   };
 
-  const setTableData = async (sections: any[]) => {
+  const updateTableData = async (sections: any[]) => {
+    const update = {
+      date: date,
+      sectionValue: gatherDataForMonth(sections),
+    };
     const response = await fetch("/api/table-data", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({
-        date: date,
-        sectionValue: gatherDataForMonth(sections),
-      }),
+      body: JSON.stringify(update),
     });
     if (response.ok) {
-      console.log("ok");
+      setTableData(generateNewTableData(update));
     }
   };
+
+  const generateNewTableData = (update: {
+    date: Date;
+    sectionValue: number;
+  }) => {
+    return tableData.map((monthData) => {
+      if (
+        monthData?.month ===
+        monthNames[date.getUTCMonth()] + " " + date.getUTCFullYear()
+      ) {
+        return { ...monthData, value: update.sectionValue };
+      }
+      return monthData;
+    });
+  };
+
+  const monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
 
   const setSections = (newSections: FinancialSectionData[]) => {
     const sortedSections = sortSections(newSections);
     setData(sortedSections);
-    setTableData(sortedSections);
+    updateTableData(sortedSections);
   };
 
   const gatherDataForMonth = (
@@ -93,7 +129,6 @@ export default function Dashboard() {
   const sortSections = (sections: FinancialSectionData[]) => {
     return [...sections].sort((a, b) => {
       const lengthDiff = a.values.length - b.values.length;
-
       if (lengthDiff !== 0) return lengthDiff;
 
       return a.title.localeCompare(b.title);
@@ -107,7 +142,6 @@ export default function Dashboard() {
     const fetchData = async () => {
       setLoading(true);
       try {
-        console.log(date.toISOString());
         const res = await fetch(`/api/sections?date=${date.toISOString()}`, {
           cache: "no-store",
         });
@@ -116,6 +150,17 @@ export default function Dashboard() {
         }
         const result = await res.json();
         setData(sortSections(result as FinancialSectionData[]));
+        const result2 = await fetch("/api/table-data", {
+          cache: "no-store",
+        });
+        if (!result2.ok) {
+          throw new Error("Failed to fetch Table Data");
+        }
+        const tableData = await result2.json();
+        if (tableData.error) {
+          throw new Error(tableData.error);
+        }
+        setTableData(tableData);
         previousDateRef.current = date;
       } catch (err) {
         console.error("Error fetching data:", err);
@@ -139,26 +184,47 @@ export default function Dashboard() {
         </div>
       </div>
       <div className="z-10 w-full h-[40rem]">
-        {loading && chartLoading ? (
-          <div className="flex justify-center items-center h-full">
-            <Tailspin size="75" stroke="5" speed="0.9" color="#f4f0e1" />
+        <div className="w-full flex items-center justify-center flex-col">
+          <div className="w-full h-full">
+            <LineChart tableData={tableData} />
           </div>
-        ) : (
-          <div className="w-full flex items-center justify-center flex-col">
-            <div className="w-full h-full">
-              <LineChart lineLoading={setChartLoading} />
+          <div className="flex items-center gap-x-4 w-full pl-4 pr-2">
+            <div className="grid-cols-2">
+              <DateInput onMonthChange={handleMonthChange} />
+              <NewSection
+                date={date}
+                onSectionAddition={addSection}
+                modalTitle="Add Account"
+                givenAsset={"ASSET"}
+                trigger={
+                  <button className="rounded-full w-10 h-10 inline-flex items-center justify-center text-sm font-medium bg-[#f4f0e1] text-black hover:bg-[#f4f0e1] m-4 hover:border-white/20 shadow-[0_0_15px_rgba(255,255,255,0.25)] hover:shadow-[0_0_20px_rgba(255,255,255,0.4)] transition-all duration-300">
+                    +
+                  </button>
+                }
+              />
             </div>
+            <button
+              className="bg-[#f4f0e1] text-black px-6 py-2 rounded-lg font-medium border border-transparent hover:border-white/20 shadow-[0_0_15px_rgba(255,255,255,0.25)] hover:shadow-[0_0_20px_rgba(255,255,255,0.4)] transition-all duration-300 mr-4 ml-auto"
+              onClick={() => setAllSectionsOpen(!allSectionsOpen)}
+            >
+              Toggle All Sections
+            </button>
+          </div>
+          {loading ? (
+            <div className="flex justify-center items-center h-full">
+              <Tailspin size="75" stroke="5" speed="0.9" color="#f4f0e1" />
+            </div>
+          ) : (
             <div className="w-full">
               <FinancialInputs
                 sections={data}
-                onMonthChange={handleMonthChange}
-                onSectionAddition={addSection}
                 date={date}
                 setSections={setSections}
+                allSectionsOpen={allSectionsOpen}
               />
             </div>
-          </div>
-        )}
+          )}
+        </div>
       </div>
     </main>
   );
