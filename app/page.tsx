84b0470c389ff7e2aecac323fcd37204fbdeba1d
Colin MacMaster <colinmac1004@gmail.com>
2025-06-06 17:40:48 -0400

    actually using ssr

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,12 @@
 import { redirect } from "next/navigation";
-import Dashboard from "./components/dashboard";
 import { auth } from "@/auth";
 
 export default async function Home() {
   const session = await auth();
   if (!session) {
     redirect("/landing");
+  } else {
+    redirect("/dashboard");
   }
-  return <Dashboard></Dashboard>;
+  return <div>test</div>;
 }
